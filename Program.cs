4d0d8e7fc7fb495872aa436893aff21220ef0230
Kishan-Ved <kishan.ved@iitgn.ac.in>
2025-03-31 03:05:51 +0530

    STT Lab 10

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class Student{
+    public string Name {get; set;}
+    public int Id {get; set;}
+    public int Marks {get; set;}
+
+    public Student(string name, int id, int marks){
+        Name = name;
+        Id = id;
+        Marks = marks;
+    }
+
+    public Student(Student s){
+        Name = s.Name;
+        Id = s.Id;
+        Marks = s.Marks;
+    }
+
+    public string GetGrade(){
+        if(Marks>=85) return "A";
+        else if(Marks>=75) return "B";
+        else if(Marks>=65) return "C";
+        return "D";
+    }
+
+    public virtual void Main(){
+        Console.WriteLine($"Name: {Name}, Id: {Id}, Marks: {Marks}, Grade: {GetGrade()}");
+    }
+}
+
+
+class Program{
+    static void Main(){
+        Student s1 = new Student("Kishan Ved", 22110122, 99);
+        Console.WriteLine("Details of object s1:");
+        s1.Main();
+
+        Student s2 = new Student(s1);
+        Console.WriteLine("Details of object s2:");
+        s2.Main();
+    }
+}
---
+++ obj/README.md
@@ -0,0 +1,4 @@
+# STT Lab 6
+Program.cs contains a C# code used in the lab report.
+
+Other C# codes used in the lab report are in .txt files.
